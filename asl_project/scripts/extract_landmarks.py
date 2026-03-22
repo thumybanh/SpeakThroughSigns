6cb078929041dadb06c1289a0d0fd065bf9df19e
mybanh <mythubanh@gmail.com>
2026-03-22 11:09:42 -0500

    initial setup - add scripts, downloaded asl alphabet dataset from Kaggle, set up a requirement txt file

---
+++ asl_project/scripts/extract_landmarks.py
@@ -0,0 +1,36 @@
+import mediapipe as mp
+import cv2
+import os
+
+
+# Mediapipe 
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode = True, max_num_hands = 1)
+
+
+# CV to read the hand images in data. 
+dataset_path = '../data/asl_alphabet/asl_alphabet_train'
+folders = os.listdir(dataset_path)
+
+
+allData = []
+for folder in folders: 
+    alphabet_folders = os.listdir(os.path.join(dataset_path, folder)) #locate each alphabet folder
+    for image in alphabet_folders: 
+        row = [folder]
+        img = cv2.imread(os.path.join(dataset_path, folder, image))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # use mediapipe to detect hand landmarks
+        result = hands.process(img_rgb)
+
+        if result.multi_hand_landmarks:
+            hand_landmarks = result.multi_hand_landmarks[0]
+            for lm in hand_landmarks.landmark: 
+                row.extend([lm.x, lm.y, lm.z])
+                allData.append(row)
+                
+
+
+
