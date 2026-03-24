5759afea67bdaa8ebfc3e21b49cac7ef6636813d
raaya <makhdumi.raaya95@gmail.com>
2026-03-23 22:56:36 -0500

    save changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while cap.isOpened():
         break
 
 cap.release()
-cv2.destryoAllWindows()
+cv2.destroyAllWindows()
 
 import numpy as np
 import uuid
@@ -32,3 +32,20 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+image = cv2.flip(image, 1)
+image.flags.writeable = False
+results = hands.process(image)
+
+image.flags.writeable = True
+image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+if results.multi_hand_landmarks:
+    for num, hand in enumerate(results.multi_hand_landmarks):
+        # to change the colors you use this mp.drawing
+        mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                  mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=4), )
+
+os.mkdir('Output images')
